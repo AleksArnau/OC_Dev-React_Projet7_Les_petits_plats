b54346f726e27b2a347d8c333b06deb61368f7e6
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2024-05-17 14:57:32 +0200

    first commit

--- src/scripts/utils/search.js
+++ src/scripts/utils/search.js
@@ -1,6 +1,7 @@
 export { searchRecipes };
 
 // Enables search by filtering recipes
+//try it out at https://jsben.ch/
 function searchRecipes(recipes, searchTerm) {
   searchTerm = searchTerm.toLowerCase();
   return recipes.filter((recipe) =>
