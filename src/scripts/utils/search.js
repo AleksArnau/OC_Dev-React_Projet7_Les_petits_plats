9d7cae4dcbd5aecc1cd1a8f168f855951158dfb8
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2024-05-17 14:59:00 +0200

    first commit

--- src/scripts/utils/search.js
+++ src/scripts/utils/search.js
@@ -4,7 +4,12 @@ export { searchRecipes };
 //try it out at https://jsben.ch/
 function searchRecipes(recipes, searchTerm) {
   searchTerm = searchTerm.toLowerCase();
-  return recipes.filter((recipe) =>
-    recipe.name.toLowerCase().includes(searchTerm)
-  );
+  const filteredRecipes = [];
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    if (recipe.name.toLowerCase().includes(searchTerm)) {
+      filteredRecipes.push(recipe);
+    }
+  }
+  return filteredRecipes;
 }
