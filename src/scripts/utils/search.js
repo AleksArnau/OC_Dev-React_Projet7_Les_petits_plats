b3f44b9be3bfa7017a8e7463cbea6f26fa49edbd
Aleksandar Arnautovic <aleksandar96@hotmail.fr>
2024-05-31 17:38:26 +0200

    search with desc and ingredients

--- src/scripts/utils/search.js
+++ src/scripts/utils/search.js
@@ -7,9 +7,25 @@ function searchRecipes(recipes, searchTerm) {
   const filteredRecipes = [];
   for (let i = 0; i < recipes.length; i++) {
     const recipe = recipes[i];
-    if (recipe.name.toLowerCase().includes(searchTerm)) {
+    if (
+      recipe.name.toLowerCase().includes(searchTerm) ||
+      recipe.description.toLowerCase().includes(searchTerm) ||
+      recipe.ingredients.some((ingredients) =>
+        ingredients.ingredient.toLowerCase().includes(searchTerm)
+      )
+    ) {
       filteredRecipes.push(recipe);
     }
   }
   return filteredRecipes;
 }
+
+//       recipe.name.toLowerCase().includes(searchTerm) ||
+//       recipe.appliance.toLowerCase().includes(searchTerm) ||
+//       recipe.description.toLowerCase().includes(searchTerm) ||
+//       recipe.ingredients.some((ingredients) =>
+//         ingredients.ingredient.toLowerCase().includes(searchTerm)
+//       ) ||
+//       recipe.ustensils.some((ustensil) =>
+//         ustensil.toLowerCase().includes(searchTerm)
+//       )
