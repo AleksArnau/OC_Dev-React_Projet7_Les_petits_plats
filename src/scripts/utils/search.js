cbb6df69e51178e8ead05f7c14100116b0b0a769
Aleksandar Arnautovic <aleksandar96@hotmail.fr>
2024-05-31 18:08:37 +0200

    search for desc and ingredients

--- src/scripts/utils/search.js
+++ src/scripts/utils/search.js
@@ -2,9 +2,16 @@ export { searchRecipes };
 
 // Enables search by filtering recipes
 //try it out at https://jsben.ch/
+
 function searchRecipes(recipes, searchTerm) {
   searchTerm = searchTerm.toLowerCase();
-  return recipes.filter((recipe) =>
-    recipe.name.toLowerCase().includes(searchTerm)
+  const filteredRecipes = recipes.filter(
+    (recipe) =>
+      recipe.name.toLowerCase().includes(searchTerm) ||
+      recipe.description.toLowerCase().includes(searchTerm) ||
+      recipe.ingredients.some((ingredients) =>
+        ingredients.ingredient.toLowerCase().includes(searchTerm)
+      )
   );
+  return filteredRecipes;
 }
